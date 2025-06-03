70fef968f701608aad6947c0703764d1860bdaa3
Henri <104088973+NgHenri@users.noreply.github.com>
2025-06-03 02:37:10 +0200

    fix: change expose port dynamic

--- Dockerfile
+++ Dockerfile
@@ -26,7 +26,7 @@ COPY backend .
 COPY backend/data ./data
 
 # Étape 8 : définition du port et de la commande de démarrage
-EXPOSE 10000
+EXPOSE $PORT
 
 # Copie du script d'entrée
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # entrypoint.sh
 
+# Utilisez le port fourni par Railway ou 10000 par défaut
+PORT=${PORT:-10000}
+
 echo "🚀 Démarrage de l'app sur le port $PORT"
-exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000}
+exec uvicorn main:app --host 0.0.0.0 --port $PORT
